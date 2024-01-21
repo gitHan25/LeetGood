4d3e1f6541d61f0b43dbe7b29ce2e4fc2ac53efe
Han25 <117625970+gitHan25@users.noreply.github.com>
2024-01-21 17:42:43 +0700

    Time: 98 ms (27.96%), Space: 49.6 MB (5.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = 0
-    let pointer = 0
+
     for (let i = 0;i<nums.length;i++){
         for (let j=i+1;j<nums.length;j++){
            
-            result = nums[pointer]+nums[j]
-                if (result == target){
-                    return [pointer,j]
-                }    
-            
+            if (nums[i]+nums[j]==target){
+                    return [i,j]
+            }
 
         }
-        pointer++
+       
 
     }
 }
