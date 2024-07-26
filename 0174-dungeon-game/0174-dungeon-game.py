1b839a664ea028afeecee22813680154ea3c6a16
Muhammad Farhan Editya <117625970+gitHan25@users.noreply.github.com>
2024-07-26 14:05:46 +0700

    Time: 47 ms (54.00%), Space: 12.7 MB (51.00%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        """
+        :type dungeon: List[List[int]]
+        :rtype: int
+        """
+        if not dungeon or not dungeon[0]:
+            return 1
+
+        m, n = len(dungeon), len(dungeon[0])
+        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
+
+        dp[m][n-1] = dp[m-1][n] = 1  # Initialize the boundary condition
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                min_health = min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]
+                dp[i][j] = max(1, min_health)
+
+        return dp[0][0]
