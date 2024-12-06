d5f35c24928d0ae9c212cad744149e49b8278a91
Muhammad Farhan Editya <117625970+gitHan25@users.noreply.github.com>
2024-12-07 06:20:49 +0700

    Time: 775 ms (32.98%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,9 +1,20 @@
+
 SELECT
     m.employee_id,
     m.name,
-    COUNT(e.reports_to) as reports_count ,
-    ROUND(AVG(e.age * 1.0), 0) as average_age 
-FROM Employees e JOIN Employees m ON e.reports_to = m.employee_id
+    COUNT(e.reports_to) as reports_count,
+    ROUND(AVG(e.age * 1.0), 0)  as average_age
+FROM (
+    SELECT
+        reports_to,
+        age
+    FROM Employees
+) e JOIN (
+    SELECT
+        employee_id,
+        name
+    FROM Employees
+) m ON e.reports_to = m.employee_id
 GROUP BY
     m.employee_id,
     m.name
