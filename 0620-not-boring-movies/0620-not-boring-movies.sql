1eb622a6fb7dd62a9ceb4b5c9ad9c303a8888e6d
Muhammad Farhan Editya <117625970+gitHan25@users.noreply.github.com>
2024-12-06 21:06:12 +0700

    Time: 276 ms (37.30%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,9 @@
-# Write your MySQL query statement below
-SELECT id, movie, description, rating
-FROM Cinema
-WHERE (id & 1) != 0 
-  AND description != 'boring'
+SELECT id, movie, description, rating 
+FROM (
+    SELECT *, ROW_NUMBER() OVER (ORDER BY id) as row_num 
+    FROM Cinema
+) as numbered_cinema
+WHERE 
+    row_num % 2 = 1 AND 
+    description != 'boring'
 ORDER BY rating DESC;
